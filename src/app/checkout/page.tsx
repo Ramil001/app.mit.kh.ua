59ff9907c85fe7fdfd6985e6bbae809a3ad33453
Ramil001 <ramil.belevtzov@gmail.com>
2024-07-19 07:51:41 +0000

    added header

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,6 +5,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Header from '@/components/Header/Header';
 
 const Item = styled( Paper )( ( { theme } ) => ( {
  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -16,21 +17,25 @@ const Item = styled( Paper )( ( { theme } ) => ( {
 
 export default function Checkout () {
  return (
-  <Box sx={{ flexGrow: 1 }}>
-   <Grid container spacing={2}>
-    <Grid item xs={12} lg={3}>
-     <Item>xs=8</Item>
+  <>
+   <Header />
+   <Box sx={{ flexGrow: 1 }}>
+    <Grid container spacing={2}>
+     <Grid item xs={12} lg={3}>
+      <Item>xs=8</Item>
+     </Grid>
+     <Grid item xs={12} lg={3}>
+      <Item>xs=4</Item>
+     </Grid>
+     <Grid item xs={12} lg={3}>
+      <Item>xs=4</Item>
+     </Grid>
+     <Grid item xs={12} lg={3}>
+      <Item>xs=8</Item>
+     </Grid>
     </Grid>
-    <Grid item xs={12} lg={3}>
-     <Item>xs=4</Item>
-    </Grid>
-    <Grid item xs={12} lg={3}>
-     <Item>xs=4</Item>
-    </Grid>
-    <Grid item xs={12} lg={3}>
-     <Item>xs=8</Item>
-    </Grid>
-   </Grid>
-  </Box>
+   </Box>
+  </>
+
  );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,30 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Box from '@mui/material/Box';
+import Link from 'next/link';
+
 export default function Header () {
  return (
-  <>
-   Header!
-  </>
- )
+  <AppBar position="static">
+   <Toolbar>
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+     <Link href="/">Main page</Link>
+    </Typography>
+    <Box>
+     <Link href="/checkout" >
+      <IconButton edge="end" color="inherit">
+       <Badge badgeContent={4} color="secondary">
+        <ShoppingCartIcon />
+       </Badge>
+      </IconButton>
+     </Link>
+    </Box>
+   </Toolbar>
+  </AppBar>
+ );
 }
