03a343741b536488a0eea05e05cee799081b88b9
Ramil001 <ramil.belevtzov@gmail.com>
2024-07-19 06:55:57 +0000

    checkout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from '@/components/Header/Header'
 
-export default function Home() {
+export default function Home () {
   return (
+
     <main className={styles.main}>
+
+      <Header></Header>
+
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
