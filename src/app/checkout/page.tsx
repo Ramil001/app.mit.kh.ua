1f8e4058bbcfaf1581f958c056822f0b5e6f185f
Ramil001 <ramil.belevtzov@gmail.com>
2024-07-19 12:17:34 +0000

    demo date update

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -23,31 +23,31 @@ const products = [
  {
   name: 'Miracle Moisture Potion',
   description: 'A magical serum that promises to make your skin feel like it just had a vacation.',
-  image: 'https://via.placeholder.com/150',
+  image: 'https://images.prom.ua/5895466173_w328_h250_krem-farba-dlya-volossya.jpg',
   price: 35.00,
  },
  {
   name: 'Glow-Up Face Mask',
   description: 'A mask that turns your face from “meh” to “wow!” in just 10 minutes.',
-  image: 'https://via.placeholder.com/150',
+  image: 'https://images.prom.ua/3410661288_w328_h250_vidnovlennya-volossya-boteks.jpg',
   price: 45.00,
  },
  {
   name: 'Kiss Me Quick Lip Balm',
   description: 'A lip balm so good, you’ll want to kiss yourself in the mirror.',
-  image: 'https://via.placeholder.com/150',
+  image: 'https://images.prom.ua/3758576376_w328_h250_keratin-dlya-slovyanskogo.jpg',
   price: 12.00,
  },
  {
   name: 'Wrinkle-Wizard Night Cream',
   description: 'A cream that works its magic while you sleep, turning wrinkles into dreams.',
-  image: 'https://via.placeholder.com/150',
+  image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR7A13HpBjw2DHtX2Bz5hdatVbqLsnXtGe1N7EBJXqy2rHSWPKOmwdQJ_sI7Qacgn7s9kO9zSk619CWK5colBNDsn-G1cXc8OxJqQ0doMYO7dF_zTfdtajgLuOGeguw-cqKRt9mjw&usqp=CAc',
   price: 60.00,
  },
  {
   name: 'Spa Day in a Jar Scrub',
   description: 'An exfoliating scrub that makes you feel like you just had a spa day without leaving your bathroom.',
-  image: 'https://via.placeholder.com/150',
+  image: 'https://images.prom.ua/3714516963_maska-dlya-rostu.jpg',
   price: 25.00,
  },
 ];
